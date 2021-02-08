81c593dac098393019292646847c83c28498271e
thomas <thomas.pepio@gmail.com>
2021-02-08 14:43:43 +0100

    Allow to start on a different port based on env var

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"os"
+
 	"github.com/thomaspepio/rest-fizzbuzz/endpoint"
 	"github.com/thomaspepio/rest-fizzbuzz/service"
 )
 
+const (
+	// PortEnvVar : name of the environment variable to specify a port to listen to
+	PortEnvVar = "PORT"
+
+	// DefaultPort : default port the server will try to use
+	DefaultPort = "8080"
+)
+
 func main() {
+	port := os.Getenv(PortEnvVar)
+	if port == "" {
+		port = DefaultPort
+	}
+
 	urlCounter := map[service.FizzBuzzRequest]int{}
 	router := endpoint.SetupRouter(urlCounter)
-	router.Run(":8080")
+	router.Run(":" + port)
 }
